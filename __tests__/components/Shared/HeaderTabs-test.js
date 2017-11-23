3c86d28435386bceadbe6ffd4f87c03b96ede55a
Jianhao Tan <jianhaot.89@gmail.com>
2017-11-24 00:03:32 +0800

    fixed headertabs test

--- __tests__/components/Shared/HeaderTabs-test.js
+++ __tests__/components/Shared/HeaderTabs-test.js
@@ -9,10 +9,10 @@ describe('HeaderTabs', () => {
     shallow = createShallow({ dive: true });
   });
 
-  it('should have 4 tabs, [Home, About, Contact, Categories]', () => {
+  it('should have 4 tabs, [Home, About, Contact, Categories, Academy]', () => {
     const wrapper = shallow(<HeaderTabs pathname="/" />);
     // console.log(wrapper.debug());
-    expect(wrapper.find('.CenteredTabs-tabs-6')).toHaveLength(4);
+    expect(wrapper.find('.CenteredTabs-tabs-6')).toHaveLength(5);
     //const text = wrapper.find('.CenteredTabs-tab-6').map(node => node.prop('label'));
     //expect(text).toEqual(['Home', 'About', 'Contact', 'Categories']);
   });
