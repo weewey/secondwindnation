a8a7a15a77e584a95a6f016dfe968198da76c604
Jianhao Tan <jianhaot.89@gmail.com>
2017-12-06 20:15:46 +0800

    implemented GA

--- pages/_document.js
+++ pages/_document.js
@@ -26,8 +26,15 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
           />
+          <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TPW822P');` }} />
         </Head>
         <body>
+          <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPW822P"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
           <Main />
           <NextScript />
         </body>
